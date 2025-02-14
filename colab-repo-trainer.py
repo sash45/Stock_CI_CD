17ba36050979a2a6b6f573c195cfeb7c7de5ea10
Sashank Vasireddy <sashankvasireddy243@gmail.com>
2025-02-14 16:48:21 -0500

    Added colab-repo-trainer.py after moving the folder

---
+++ colab-repo-trainer.py
@@ -0,0 +1,13 @@
+import requests
+
+colab_url='https://colab.research.google.com/drive/1GZbP2btHQO8KGyqxE1Kv1m85Ra3HnzFw?usp=sharing'
+
+def trigger_colab_execution():
+    response = requests.get(colab_url)
+    if response.status_code == 200:
+        print("Colab notebook execution triggered successfully.")
+    else:
+        print(f"Failed to trigger Colab notebook. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    trigger_colab_execution()
